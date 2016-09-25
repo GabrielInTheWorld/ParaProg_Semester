6d1fe700dce48454b494ee1ef162157eecf74f15
GabrielInTheWorld <meyergabriel@live.de>
2016-09-25 23:19:14 +0200

    initialize methods

--- ParaProg_Semester/src/com/gabrielmeyer/process/Process.java
+++ ParaProg_Semester/src/com/gabrielmeyer/process/Process.java
@@ -1,64 +1,125 @@
 package com.gabrielmeyer.process;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.CountDownLatch;
 
 import com.gabrielmeyer.node.Node;
+import com.gabrielmeyer.node.NodeAbstract;
 
-public class Process implements Node {
+public class Process extends NodeAbstract {
 
-	private boolean initiator = false;
-	private List<Node> neighbours = new ArrayList<Node>();
-	// private List<Node> copyOfNeighbours = new ArrayList<Node>();
-	private Node noteNode = null;
+	private boolean running = false, suspending = false;
+	private Set<Node> noteNodes = new HashSet<>();
 
-	public Process(boolean initiator) {
-		this.initiator = initiator;
+	public Process(String name, boolean initiator, CountDownLatch startLatch) {
+		super(name, initiator, startLatch);
 	}
 
 	@Override
 	public void hello(Node neighbour) {
-		if (unknown(neighbour)) {
-			neighbours.add(neighbour);
-		}
+		System.out.println(this.toString() + " get greetings from: " + neighbour.toString());
+		neighbours.add(neighbour);
 	}
 
 	@Override
 	public void wakeup(Node neighbour) {
-		noteNode = neighbour;
-		for (Node node : neighbours) {
-			if (!neighbour.equals(node)) {
-				node.wakeup(this);
+		noteNodes.add(neighbour);
+		synchronized (this) {
+			if (!this.isAlive()) {
+				System.out.println("this.node: " + this.toString() + " woke up by: " + neighbour.toString());
+				for (Node node : neighbours) {
+					System.out.println(this.toString() + " neighbours: " + node.toString());
+				}
+				System.out.println(this.toString() + " wakeUp from " + neighbour.toString());
+				// Thread t = new Thread(this);
+				suspending = true;
+				this.start();
+				// suspending = true;
 			}
 		}
 	}
 
 	@Override
 	public void echo(Node neighbour, Object data) {
-		if (noteNode != null)
-			neighbours.remove(noteNode);
-		for (Node node : neighbours) {
-			if (neighbour.equals(node)) {
-				neighbours.remove(node);
-			}
-		}
-		if (neighbours.isEmpty() && noteNode != null) {
-			noteNode.echo(this, data);
-		}
+		System.out.println(this.toString() + " echo from " + neighbour.toString());
+		noteNodes.remove(neighbour);
+		if (noteNodes.isEmpty())
+			suspending = false;
+		// notifyAll();
 	}
 
-	/**
-	 * verify if the given neighbor is unknown to this node
-	 * 
-	 * @param neighbour
-	 * @return true if the neighbor is unknown
-	 */
-	private boolean unknown(Node neighbour) {
-		for (Node node : neighbours) {
-			if (neighbour.equals(node)) {
+	@Override
+	public void run() {
+		System.out.println("initiator?: " + initiator);
+		if (initiator) {
+			try {
+				System.out.println("before await()");
+				startLatch.await();
+				System.out.println("after await()");
+				for (Node node : neighbours) {
+					node.wakeup(this);
+					noteNodes.add(node);
+				}
+				suspending = true;
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		synchronized (this) {
+			if (!initiator)
+				for (Node node : neighbours) {
+					if (!noteNodes.contains(node)) {
+						node.wakeup(this);
+					} else {
+						suspending = false;
+					}
+				}
+			try {
+
+				while (suspending) {
+					System.out.println(this.toString() + " waiting() " + isSuspending());
+					wait();
+				}
+				System.out.println(this.toString() + ": is not waiting anymore");
+				for (Node node : noteNodes) {
+					node.echo(this, null);
+				}
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		System.out.println(this.toString() + ": run() ends");
+	}
+
+	// public boolean isRunning() {
+	// return running;
+	// }
+
+	public boolean isSuspending() {
+		return suspending;
+	}
+
+	public boolean notWaker(Node node) {
+		for (Node n : noteNodes) {
+			if (node.equals(n)) {
 				return false;
 			}
 		}
 		return true;
+	}
+
+	@Override
+	public void setupNeighbours(Node... neighbours) {
+		System.out.println(this.toString() + " neighbours: " + neighbours.toString());
+		// TODO improve method
+		for (Node neighbour : neighbours) {
+			System.out.println("setupNeighbour: " + neighbour.toString());
+			this.neighbours.add(neighbour);
+			neighbour.hello(this);
+		}
+		// if (!initiator) {
+		startLatch.countDown();
+		// }
 	}
 }
---
+++ ParaProg_Semester/src/com/gabrielmeyer/process/StartApplication.java
@@ -0,0 +1,127 @@
+package com.gabrielmeyer.process;
+
+import java.util.concurrent.CountDownLatch;
+
+public class StartApplication {
+
+	CountDownLatch startLatch;
+
+	Process[] processors;
+
+	public StartApplication(String kindOfGraph, int count) {
+		processors = new Process[count];
+		startLatch = new CountDownLatch(count / 2);
+		switch (kindOfGraph) {
+		case "C":
+			startCircle(count);
+			break;
+		case "K":
+			startFullGraph(count);
+			break;
+		case "BT":
+			startBinaryTree(count);
+			break;
+		case "T":
+			startTree(count);
+			break;
+		case "L":
+			startLoop(count);
+			break;
+		default:
+			break;
+		}
+		// Process[] processors = new Process[count];
+		// for (int i = 0; i < count; ++i) {
+		// processors[i] = new Process(String.valueOf(i), false, startLatch);
+		// }
+		// processors[count / 2] = new Process(String.valueOf(count / 2), true,
+		// startLatch);
+		// Thread t = new Thread(processors[count / 2]);
+		// t.start();
+		// processors[count / 2].setupNeighbours(processors[count / 2 - 2],
+		// processors[count / 2 + 2]);
+		// processors[count / 4].setupNeighbours(processors[count / 2 - 1],
+		// processors[count / 2 - 3]);
+		// processors[count / 2 + 2].setupNeighbours(processors[count / 2 + 1],
+		// processors[count / 2 + 3]);
+
+		// for (int i = 1; i < count; ++i) {
+		// processors[i] = new Process(String.valueOf(i), false, startLatch);
+		// }
+
+		// for (int i = 0; i < processors.length; ++i) {
+		// // processors[i].setupNeighbours(processors[(i + 1) % count]);
+		// }
+
+	}
+
+	/**
+	 * 
+	 * @param count
+	 */
+	private void startCircle(int count) {
+		// startLatch = new CountDownLatch(count);
+
+		// initialize initiator
+		processors[0] = new Process("0", true, startLatch);
+
+		// initialize other processes
+		for (int i = 1; i < count; ++i) {
+			processors[i] = new Process(String.valueOf(i), false, startLatch);
+		}
+
+		// initiator starts
+		processors[0].start();
+
+		// setup neighbours
+		for (int i = 0; i < processors.length; ++i) {
+			processors[i].setupNeighbours(processors[(i + 1) % count]);
+		}
+	}
+
+	private void startFullGraph(int count) {
+		// startLatch = new CountDownLatch(count);
+
+		processors[0] = new Process("0", true, startLatch);
+
+		for (int i = 1; i < count; ++i) {
+			processors[i] = new Process(String.valueOf(i), false, startLatch);
+		}
+
+		processors[0].start();
+	}
+
+	private void startBinaryTree(int count) {
+		// startLatch = new CountDownLatch(count / 2);
+		Process[] processors = new Process[count];
+		for (int i = 0; i < count; ++i) {
+			processors[i] = new Process(String.valueOf(i), false, startLatch);
+		}
+		processors[count / 2] = new Process(String.valueOf(count / 2), true, startLatch);
+		Thread t = new Thread(processors[count / 2]);
+		t.start();
+		processors[count / 2].setupNeighbours(processors[count / 2 - 2], processors[count / 2 + 2]);
+		processors[count / 4].setupNeighbours(processors[count / 2 - 1], processors[count / 2 - 3]);
+		processors[count / 2 + 2].setupNeighbours(processors[count / 2 + 1], processors[count / 2 + 3]);
+	}
+
+	private void startTree(int count) {
+		processors[0] = new Process("0", true, startLatch);
+
+		for (int i = 1; i < count; ++i) {
+			processors[i] = new Process(String.valueOf(i), false, startLatch);
+		}
+	}
+
+	private void startLoop(int count) {
+
+	}
+
+	public static void main(String[] args) {
+		for (String a : args) {
+			System.out.println("a: " + a);
+		}
+		new StartApplication(args[0], Integer.valueOf(args[1]));
+	}
+
+}
